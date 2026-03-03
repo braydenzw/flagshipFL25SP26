174841011af290ebe4d5d2657d2898c9be657706
Ethan Roberts <e.roberts@gmail.com>
2026-03-03 11:12:26 -0600

    working on enemy movement

--- Assets/Scripts/Monster/EnemyMovementControllerRevamped.cs
+++ Assets/Scripts/Monster/EnemyMovementControllerRevamped.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI; 
+using UnityEngine.AI;
 
-// author: Ethan Roberts
-// if it's a buncha junk, I apologize in advance
+/// <summary>
+/// author: Ethan Roberts
+/// if it's a buncha junk, sorry mate
+/// 
+/// 
+/// </summary>
 public class EnemyMovementControllerRevamped : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private Transform target; 
+    private Transform target; // this is what the enemy is set to pursue
+    private Transform player;
+    private enum states
+    {
+        searching,
+        chasing,
+        wandering
+    }
+
+    // use to define behavior categories
+    private states state = states.searching;
+
+    // parameters
+    private float chase_time = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +38,73 @@ public class EnemyMovementControllerRevamped : MonoBehaviour
             Debug.Log("enemy does not have a NavMeshAgent attached");
         }
 
-        // set target to player
-        target = GameObject.FindWithTag("Player").transform;
-        if (target == null)
-        {
-            Debug.Log("enemy could not find player");
-        }
+        player = GameObject.FindWithTag("Player").transform;
+
+        state = states.wandering;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        switch (state)
+        {
+            case states.searching:
+                break;
+
+            case states.wandering:
+                
+                break;
+
+            case states.chasing:
+                StartCoroutine(ChasePlayer());
+                break;
+
+            default:
+                Debug.Log("enemy invalid state (logic prob scuffed)");
+                break;
+        }
+
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void setTargetToPlayer(Transform player)
+    {
+        if (player == null)
+        {
+            Debug.Log("enemy could not find player; check if player is tagged");
+        }
+
+        target = player;
+    }
+
+
+    // returns a target vector 
+    private Vector3 Wander(float radius)
+    {
+        // find ran dir in unit sphere, scale by radius
+        Vector3 dir = Random.insideUnitSphere * radius;
+        // make dir relative to agent position
+        dir += agent.transform.position;
+        NavMeshHit hit;
+        Vector3 target = Vector3.zero;
+        
+        // get the closest point on the NavMesh
+        if (NavMesh.SamplePosition(dir, out hit, radius, NavMesh.AllAreas))
+        {
+            target = hit.position;
+        }
+
+        return target;
+    }
+
+    private IEnumerator ChasePlayer() {
+        Debug.Log("enemy started chasing player");
+        setTargetToPlayer(player);
+        yield return new WaitForSeconds(chase_time);
+        target = null;
+        state = states.wandering;
     }
 }
