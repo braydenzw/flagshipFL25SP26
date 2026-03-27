6c98a63e0b1a23505838781eb10ed2a97e1a2ebe
Rainbowzilla <xwingstarfighter301@gmail.com>
2026-03-27 15:25:11 -0700

    Interactable Door
    -3D Modelled Door
    -Added Door Open/Close Logic
    -Prompt Message
     -Open Door
     -Close Door
    -Added Box Collider to Door Gameobject
    -Changed Door's Layer to Interactable Layer
    -Animated Door Opening

---
+++ Assets/Scripts/Nick's Scripts/Interactable Objects/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    [SerializeField] private string customCloseMessage;
+    [SerializeField] private Animator animator;
+
+    private bool isOpen = false;
+    private string originalPrompt;
+
+    void Start()
+    {
+        originalPrompt = promptMessage;
+    }
+
+    protected override void Interact()
+    {
+        isOpen = !isOpen;
+        animator.SetBool("IsOpen", isOpen);
+
+        if (isOpen)
+        {
+            promptMessage = customCloseMessage;
+        }
+        else
+        {
+            promptMessage = originalPrompt;
+        }
+    }
+}
